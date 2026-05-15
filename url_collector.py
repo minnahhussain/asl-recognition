751c38b63194e412165d062aa9290d18275d64f1
Minnah Hussain <hussainminnah@gmail.com>
2026-05-15 12:23:35 -0400

    Downloading asl video files. Skipping all invalid/corrupted videos.

--- url_collector.py
+++ url_collector.py
@@ -1,4 +1,5 @@
 import json
+import requests
 #Values searching for:
 target_urls = {"yes":[], "no":[], "please":[], "thank you":[]}
 
@@ -21,3 +22,19 @@ with open ("WLASL_v0.3.json", 'r') as file:
                     target_urls[gloss_value].append(url)
 for target, urls in target_urls.items():
     print(f"URLS for values: '{target}':{urls}")
+
+#Saving urls of each sign into a file
+for key in target_urls:
+    for i, value in enumerate(target_urls[key]):
+        #Creating file
+        with open (key + str(i) + ".mp4", "wb") as file:
+            try:
+                x = requests.get(value)
+                #ensuring video works
+                if x.status_code == 200:
+                    file.write(x.content)
+                else:
+                    print(f"Skipping {value}: status {x.status_code}")
+            except Exception as e:
+                 print(f"Skipping {value}: {e}")
+                
